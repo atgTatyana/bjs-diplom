12d276041794424ffcec148a9803a7ea13e586fe
Tatyana Amosova <atgatg@bk.ru>
2023-03-06 19:51:22 +0300

    LoginPage

---
+++ public/loginPage.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = data => {
+	// data - это пользовательский ввод { login, password }
+	ApiConnector.login(data, response => {
+		// возвращает объект {success, error}
+		if (response.success === true) {
+			location.reload();							// обновить страницу
+		} else {
+			userForm.setLoginErrorMessage(response.error);
+		}
+	})
+}
+
+userForm.registerFormCallback = data => {
+	ApiConnector.register(data, response => {
+		if (response.success === true) {
+			location.reload();
+		} else {
+			userForm.setRegisterErrorMessage(response.error);
+		}
+	})
+}
+
+
+// ApiConnector.login({login: "ivan@demo.ru", password: "demo"}, response => console.log(response));
+// ApiConnector.logout(response => console.log(response));
+// userForm.loginFormCallback = data => console.log(data);
